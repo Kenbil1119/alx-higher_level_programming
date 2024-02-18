c86f711aca9318045010c7bbf0e071ba0aa7c105
Usman Saheed .K <uskehinde@gmail.com>
2024-02-18 13:00:13 +0000

    Decimal and Hexadecimal of numbers

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# 'd' been the format type of integer (decimal)
+# 'x' been the format type of hexadecimal
+
+for num in range(0, (98 + 1)):
+    print("{:d} = 0x{:x}".format(num, num))
