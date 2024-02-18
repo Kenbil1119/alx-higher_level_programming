25b54519180647c6600392a3beaf3adf46dee3f4
Usman Saheed .K <uskehinde@gmail.com>
2024-02-18 13:29:33 +0000

    formatting in two digit, Usage of 'break' keyword in Python

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(0, (9 + 1)):
-    for j in range(0, (9 + 1)):
-        if (i == 9) and (j == 9):
-            print("{:d}{:d}".format(i, j))
-            break
-        print("{:d}{:d}".format(i, j), end=", ")
+for i in range(100):
+    if i == 99:
+        print("{:02d}".format(i))
+        break
+    print("{:02d}".format(i), end=", ")
