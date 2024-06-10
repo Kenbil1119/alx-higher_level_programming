57f0221d1b04669ec1e4e6a7b928e6659bee59f6
Usman Saheed .K <uskehinde@gmail.com>
2024-06-10 21:55:44 +0000

    Print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+Write a function that prints the last digit of a number.
+
+Prototype: def print_last_digit(number):
+    Returns the value of the last digit
+    You are not allowed to import any module
+    You don’t need to understand __import__
+'''
+
+def print_last_digit(num):
+
+    print(int(num) % 10)
+    return int(num) % 10
---
+++ 0x01-python-if_else_loops_functions/9-test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+lastdigit = __import__ ('9-print_last_digit').print_last_digit
+
+lastdigit("300")
+lastdigit("63114")
+lastdigit(32)
