2d4ab8c5dbfb72e9d25b692c41fa63ed6a33f26c
Usman Saheed .K <uskehinde@gmail.com>
2024-06-10 23:00:53 +0000

    Lowercase to uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,30 +4,24 @@
 Write a function that prints a string in uppercase followed by a new line.
 
 Prototype: def uppercase(str):
-    Using only no more than 2 print functions with string format
-    Use only one loop in the code
-    Not allowed to import any module
-    Not allowed to use str.upper() and str.isupper()
+    You can only use no more than 2 print functions with string format
+    You can only use one loop in your code
+    You are not allowed to import any module
+    You are not allowed to use str.upper() and str.isupper()
     Tips: ord()
+    You don’t need to understand __import__
 '''
 # Author: Usman Saheed .K
 
 
 def uppercase(str):
 
-    # Empty string to reserve space for uppercase of str
+    dif = ord('a') - ord('A')
     upper_str = ""
-
-    # Different between Uppercase and Lowercase
-    dif = ord("a") - ord("A")
-
-    # Loop through characters of str
     for char in str:
-        # check each characters if it is Lowercase, Hence convert to uppercase using the 'dif' if condition True
-        if char >= 'a' and char <= 'z':
-            upper_str += (chr(ord(char) - dif))
+        if 'a' <= char <= 'z':
+            upper_str += chr(ord(char) - dif)
         else:
             upper_str += char
 
-        # Print uppercase version of str
-    print("{}".format(upper_str))
+    print('{}'.format(upper_str))
