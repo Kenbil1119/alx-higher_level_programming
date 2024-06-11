1a56699ed6d065e518d761c6fa783669571edf3b
Usman Saheed .K <uskehinde@gmail.com>
2024-06-11 10:53:50 +0000

    a ** b

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#/usr/bin/python3
+
+'''
+Write a function that computes a to the power of b and return the value.
+
+Prototype: def pow(a, b):
+    Returns the value of a ^ b
+    You are not allowed to import any module
+    You don’t need to understand __import__
+'''
+# Author: Usman Saheed .K
+
+
+def pow(a, b):
+    return int(a) ** int(b)
---
+++ 0x01-python-if_else_loops_functions/11-test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+_pow = __import__ ('11-pow')._pow
+print("3³ is: ", end="")
+_pow(3, 3)
+
+print("10² us: ", end="")
+_pow(10, 2)
